fb7dde0d23c5fdb52677979a4e6db6c97a5aa131
Diego <belov-93201093@mail.ru>
2019-12-03 04:00:35 +0300

    Updates

---
+++ precache-manifest.ac46b4489d583ab48bd909558d00fc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a81c4f0b79e6d6da094a040e9c63010",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "438ecf82010c46d45dc9",
+    "url": "/notes/static/css/main.861e5742.chunk.css"
+  },
+  {
+    "revision": "c34cbe9540926faeae83",
+    "url": "/notes/static/js/2.c73c3378.chunk.js"
+  },
+  {
+    "revision": "438ecf82010c46d45dc9",
+    "url": "/notes/static/js/main.61fb01d9.chunk.js"
+  },
+  {
+    "revision": "a6c780690345bf7c0b60",
+    "url": "/notes/static/js/runtime-main.5614885a.js"
+  }
+]);
