6fe8628d0a21784f0fd85bc5f341662fbae90bb6
Diego <belov-93201093@mail.ru>
2019-12-03 03:51:14 +0300

    init commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+
+export  const About = () => (
+	<div className="jumbotron">
+		<div className="container">
+			<h1 className="display-4">Записывай, не забывай...</h1>
+			<p className="lead">Привычка записывать – главный секрет хорошей памяти.</p>
+		</div>
+	</div>
+)
